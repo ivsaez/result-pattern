7aece6e1092fcdc9a321b80a26ccce95e2c86352
Ivan Saez Zamora <ivsaez@gmail.com>
2022-05-25 22:59:35 +0200

    implement result pattern

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,70 @@
-function hello(name: string = "Ivan"): string {
-  return `Hello, ${name}`;
-}
+export class Result<T> {
+  private _success: boolean;
+  private _failure: boolean;
+  private _error: string;
+  private _value: T;
 
-export default hello;
+  private constructor (success: boolean, error?: string, value?: T) {
+    if (success && error)
+      throw new Error("Successful doesn't need error message.");
+
+    if (!success && !error)
+      throw new Error("Failure result needs error message.");
+
+    this._success = success;
+    this._failure = !success;
+    this._error = error;
+    this._value = value;
+  }
+
+  get success(){
+    return this._success;
+  }
+
+  get failure(){
+    return this._failure;
+  }
+
+  get error(){
+    if(this._error == null) 
+      return "";
+    
+    return this._error;
+  }
+
+  get value(){
+    if (!this._success)
+      throw new Error('The result is a failure.');
+
+    return this._value;
+  }
+
+  public static ok<T>(value?: T) : Result<T> {
+    return new Result<T>(true, null, value);
+  }
+
+  public static error<T>(error: string): Result<T> {
+    return new Result<T>(false, error);
+  }
+
+  public static combine(results: Result<any>[]) : Result<any> {
+    for (let result of results)
+      if (result.failure) 
+        return result;
+    
+    return Result.ok<any>();
+  }
+
+  public static combineAll(results: Result<any>[]) : Result<any> {
+    let messages: string[] = [];
+    
+    for (let result of results)
+      if (result.failure) 
+        messages.push(result.error);
+    
+    if(messages.length > 0)
+      return Result.error<any>(messages.join(' '));
+    
+    return Result.ok<any>();
+  }
+}
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,11 +1,69 @@
-import hello from "..";
+import { Result } from "../index";
 
-describe("it should say hello", () => {
-  it("should greet 'Ivan'", () => {
-    expect(hello()).toBe("Hello, Ivan");
+describe("Result should", () => {
+  it("create a success", () => {
+    let success = Result.ok("OK");
+
+    expect(success.success).toBe(true);
+    expect(success.failure).toBe(false);
+    expect(success.value).toBe("OK");
+    expect(success.error).toBe("");
   });
 
-  it("should greet 'Daniel'", () => {
-    expect(hello("Daniel")).toBe("Hello, Daniel");
+  it("create an error", () => {
+    let failure = Result.error("WRONG");
+
+    expect(failure.success).toBe(false);
+    expect(failure.failure).toBe(true);
+    expect(failure.error).toBe("WRONG");
+    expect(() => failure.value).toThrowError();
+  });
+
+  it("combine success", () => {
+    let success = Result.ok("OK");
+    let anotherSuccess = Result.ok("SUPER");
+
+    let combined = Result.combine([ success, anotherSuccess ]);
+
+    expect(combined.success).toBe(true);
+    expect(combined.failure).toBe(false);
+    expect(combined.value).toBe(undefined);
+    expect(combined.error).toBe("");
+  });
+
+  it("combine all success", () => {
+    let success = Result.ok("OK");
+    let anotherSuccess = Result.ok("SUPER");
+
+    let combined = Result.combineAll([ success, anotherSuccess ]);
+
+    expect(combined.success).toBe(true);
+    expect(combined.failure).toBe(false);
+    expect(combined.value).toBe(undefined);
+    expect(combined.error).toBe("");
+  });
+
+  it("combine error", () => {
+    let failure = Result.error("WRONG");
+    let anotherFailure = Result.error("WRONG AGAIN");
+
+    let combined = Result.combine([ failure, anotherFailure ]);
+
+    expect(combined.success).toBe(false);
+    expect(combined.failure).toBe(true);
+    expect(combined.error).toBe("WRONG");
+    expect(() => failure.value).toThrowError();
+  });
+
+  it("combine all error", () => {
+    let failure = Result.error("WRONG");
+    let anotherFailure = Result.error("WRONG AGAIN");
+
+    let combined = Result.combineAll([ failure, anotherFailure ]);
+
+    expect(combined.success).toBe(false);
+    expect(combined.failure).toBe(true);
+    expect(combined.error).toBe("WRONG WRONG AGAIN");
+    expect(() => failure.value).toThrowError();
   });
 });
